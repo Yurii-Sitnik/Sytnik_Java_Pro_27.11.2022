6247414a7126eef452a74f844893f1aca0170d87
Sytnyk Yurii <yoursit1980@gmail.com>
2022-12-04 13:49:45 +0200

    homework1

--- src/homework1.java
+++ src/homework1.java
@@ -1,2 +1,13 @@
+<<<<<<< HEAD
 package PACKAGE_NAME;public class homework1 {
+=======
+public class homework1 {
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Java Pro the best group in Hillel");
+            System.out.println("Hillel is a super school))");
+        }
+    }
+
+>>>>>>> 7a92d70 (homework1)
 }
